0d03fd03a4ba0b1b16be7f90707b5ffb7cb1f029
Egor Pokumin <89711006+EgorLeMeilleur@users.noreply.github.com>
2022-01-25 16:16:40 +0500

    Add files via upload

---
+++ first_term_mipt/first_contest/1_5.cpp
@@ -0,0 +1,20 @@
+﻿//Вывести квадраты натуральных чисел от 1 до n, используя только O(n) операций сложения и вычитания(умножением пользоваться нельзя).n ≤ 1000.
+#include <iostream>
+int out_squares(int number)
+{
+	int prev = 1, squares = 1;
+	for (int i = 0; i < number - 1; i++)
+	{
+		std::cout << squares << " ";
+		prev += 2; // квадрат n можно получить, сложив n первых нечетных чисел
+		squares += prev;
+	}
+	return squares;
+}
+int main()
+{
+	int number;
+	std::cin >> number;
+	std::cout << out_squares(number);
+	return 0;
+}
